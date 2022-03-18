90d7b0d904a55e8ce7653d4017e3bcd51decdc21
Narcis Lazar <lnarcis310@gmail.com>
2022-03-18 20:20:56 +0200

    updates

--- config.php
+++ config.php
@@ -1,8 +1,19 @@
 <?php
-    $username = "root";
-    $database = "tic-tac-toe-live";
-    $server = "localhost";
-    $password = "";
+    $whitelist = array(
+		'127.0.0.1',
+		'::1'
+	);
+	if (!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+		$username = getenv('USERNAME');
+		$password = getenv('PASSWORD');
+		$database = getenv('DATABASE');
+		$server = getenv('SERVER');
+	} else {
+		$username = "root";
+		$password = "";
+		$database = "tic-tac-toe-live";
+		$server = "localhost";
+	}
 
     $sql = mysqli_connect($server, $username, $password, $database);
     if (!$sql) {
